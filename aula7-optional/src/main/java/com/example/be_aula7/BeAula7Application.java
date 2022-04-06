d616e622b0f237e0c98dc87f4570395fa7d45323
edunuvem <eduardo.carvalho@nuvemshop.com.br>
2022-04-06 18:16:21 -0300

    aulas 10 e 11 commit 1

--- aula7-optional/src/main/java/com/example/be_aula7/BeAula7Application.java
+++ aula7-optional/src/main/java/com/example/be_aula7/BeAula7Application.java
@@ -2,8 +2,10 @@ package com.example.be_aula7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BeAula7Application {
 
 	public static void main(String[] args) {
